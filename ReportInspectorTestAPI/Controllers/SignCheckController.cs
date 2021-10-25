112946b751359766c9492aa5217c9565243295c9
Catie Ladd <catherine.ladd@nuance.com>
2021-10-25 11:34:49 -0400

    Add project files.

---
+++ ReportInspectorTestAPI/Controllers/SignCheckController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportInspectorTestAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class SignCheckController : ControllerBase
+	{
+		private readonly ILogger<SignCheckController> _logger;
+
+		public SignCheckController(ILogger<SignCheckController> logger)
+		{
+			_logger = logger;
+		}
+
+
+		[HttpPost]
+		public ICommandResponse CheckReport(Report report)
+		{
+			var text = report.Text.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(n => n.Trim().ToLowerInvariant()).ToList();
+
+			if (text.Contains("ok")) return new OkToSignResponse();
+
+			var response = new BlockSignResponse();
+			if (text.Contains("warn"))
+			{
+				response.Items.Add(new RequestForInformation
+				{
+					Id = "1",
+					Severity = BlockSeverity.Warn,
+					Display = "Warning Text Here"
+				});
+			}
+			if (text.Contains("critical"))
+			{
+				response.Items.Add(new RequestForInformation
+				{
+					Id = "2",
+					Severity = BlockSeverity.Critical,
+					Display = "Critical Text Here"
+				});
+			}
+			if (text.Contains("info"))
+			{
+				response.Items.Add(new RequestForInformation
+				{
+					Id = "3",
+					Severity = BlockSeverity.Info,
+					Display = "Informational Text Here"
+				});
+			}
+
+			return response;
+		}
+	}
+}
---
+++ ReportInspectorTestAPI/ModelClasses.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ReportInspectorTestAPI
+{
+	public interface ICommandResponse
+	{
+		string OnSignResponse { get; }
+		IList<RequestForInformation> Items { get; set; }
+	}
+
+	public class OkToSignResponse : ICommandResponse
+	{
+		public string OnSignResponse { get; } = "okToSign";
+
+		public IList<RequestForInformation> Items { get; set; } = null;
+	}
+
+	public class BlockSignResponse : ICommandResponse
+	{
+		public string OnSignResponse { get; } = "blockSign";
+
+		public IList<RequestForInformation> Items { get; set; } = new List<RequestForInformation>();
+	}
+
+	public class RequestForInformation
+	{
+		public string Id { get; set; }
+		public string Display { get; set; }
+		public BlockSeverity Severity { get; set; }
+	}
+
+	public enum BlockSeverity
+	{
+		Info,
+		Warn,
+		Critical
+	}
+
+	public class Report
+	{
+		public string Text { get; set; }
+		public PatientInfo Patient { get; set; }
+	}
+
+	public class PatientInfo
+	{
+		public string Sex { get; set; }
+		public DateTime dob { get; set; }
+	}
+}
