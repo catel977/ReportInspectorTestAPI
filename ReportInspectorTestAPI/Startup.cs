a683bb8ae93d9bd62c8cff1b0c6bfa7cb352627a
Catie Ladd <catherine.ladd@nuance.com>
2021-10-26 13:55:23 -0400

    Updated swagger version and moved code so that swagger appears on release deploys as well.

--- ReportInspectorTestAPI/Startup.cs
+++ ReportInspectorTestAPI/Startup.cs
@@ -48,9 +48,10 @@ namespace ReportInspectorTestAPI
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
-				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReportInspectorTestAPI v1"));
 			}
+
+			app.UseSwagger();
+			app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReportInspectorTestAPI v1"));
 
 			app.UseHttpsRedirection();
 
